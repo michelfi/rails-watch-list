ad64abb3cafa7922beb66966f5e5cf02b21ee82b
michelfi <michel.figueres@gmail.com>
2023-08-24 11:49:41 +0200

    oui

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :movie_id, uniqueness: { scope: :list_id, message "hello" }
+  validates :movie_id, uniqueness: { scope: :list_id, message: "hello" }
   validates :comment, length: { minimum: 6 }
 end

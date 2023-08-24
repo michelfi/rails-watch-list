d92d37ef9645660fc0f5e78f1d8677048541dbbf
michelfi <michel.figueres@gmail.com>
2023-08-24 15:41:42 +0200

    oui

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,4 +2,20 @@ class BookmarksController < ApplicationController
   def new # GET
     @bookmark = Bookmark.new
   end
+
+  def create # POST
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to @bookmark, notice: "list was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+
 end
